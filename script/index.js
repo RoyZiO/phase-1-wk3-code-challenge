2cad60b8b2992152af919f9859cee77f042017ff
RoyZiO <nziokinroy@gmail.com>
2022-12-12 21:26:31 +0300

    Getting first movie

--- script/index.js
+++ script/index.js
@@ -1,14 +1,14 @@
 
 
 print =  (value) => {console.log(value)}
-fetch('http://localhost:3000/films')
-// print(res)
-.then(res=>(res.json()))
-.then(data=> {
-    //console.log(data.films[0]);
-     data=data.films
-    // selector for our first movie poster
-    image = document.querySelector('img.rounded-float-start')
+
+function fetchMovies() {
+    fetch("http://localhost:3000/films")
+
+    .then(response => (response.json()))
+    .then(data => {
+        data.films 
+        image = document.querySelector('img.card-img-top')
     title = document.querySelector('h5.card-title')
     description = document.querySelector('p.card-text')
     info = document.querySelectorAll('li.list-group-item')
@@ -19,29 +19,32 @@ fetch('http://localhost:3000/films')
     description.textContent = data[0].description
     info[0].textContent = `Showtime =   ${data[0].showtime}`
     info[1].textContent = `Runtime =   ${data[0].runtime} mins`
-    info[2].textContent = `Tickets-Sold =  ${data[0].tickets_sold}`
-    remainder = (data[0].capacity)-(data[0].tickets_sold)
-    footer.textContent = `tickets-Remaining:${remainder}`  
-})
+    info[2].textContent = `Available-Tickets =  ${(data[0].capacity)- (data[0].tickets_sold)}`
+    //remainder = (data[0].capacity)-(data[0].tickets_sold)
+    //footer.textContent = `tickets-Remaining:${remainder}`  
 
+    })
+}
+fetchMovies();
 
-// side navigation menu bar content displayer
-fetch('http://localhost:3000/films')
-.then(res=>(res.json()))
+function sideNavBar() {
+
+    fetch('http://localhost:3000/films')
+.then(response => (response.json()))
 
 .then(data=>{
 
  let i = 0
  do{
   i+=1
-  data2= data.films[i]
+  data2 = data.films[i]
   // data =data.films;print(data[0].title)
-  const side = document.querySelectorAll('a.others'); 
+  const side = document.querySelectorAll('a#others'); 
   // print (side[i])
   side[i].textContent = data2.title
 //  print(data2.title)
  }while(i<14)   
- const side = document.querySelectorAll('a.others'); 
+ const side = document.querySelectorAll('a#others'); 
  print (side[0].textContent)
   })
   // side bar navigation menu displayer
@@ -60,15 +63,28 @@ function openNav() {
   });
   print(`hello`)
 
-  const as = document.querySelectorAll('a.others');print(as)
-  addEventListener('onClick',()=>{
-  print("done")
-  fetch('http://localhost:3000/films')
-.then(res=>(res.json()))
-.then(data=>{
-  print(data)
+}
+sideNavBar();
+
+
+
+function otherMovies() {
+    const as = document.querySelectorAll('a.others');print(as)
+    addEventListener('onClick',()=>{
+    print("done")
+    fetch('http://localhost:3000/films')
+  .then(res=>(res.json()))
+  .then(data=>{
+    print(data)
+    
+     
+    })
   
-   
   })
 
-})
+} 
+otherMovies();
+  
+
+
+// side navigation menu bar content displayer
